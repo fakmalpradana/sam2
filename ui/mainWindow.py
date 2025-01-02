6eee8e1dcfcee8e4987b3ea0753341e8bc8aafc6
Fairuz Akmal Pradana <fakmalpradana@gmail.com>
2025-01-02 21:49:30 +0700

    add toolbar for input and save

--- ui/mainWindow.py
+++ ui/mainWindow.py
@@ -6,13 +6,13 @@ from PyQt5.QtWidgets import (
 from PyQt5.QtGui import QPixmap, QImage, QPainter, QColor, QPen
 from PyQt5.QtCore import Qt, QPoint
 import numpy as np
-# import cv2
+import cv2
 
 class MainWindow(QMainWindow):
     def __init__(self):
         super().__init__()
 
-        self.setWindowTitle("SAM Interactive Digitizer")
+        self.setWindowTitle("SAM-2 Interactive Digitizer")
         self.setGeometry(100, 100, 800, 600)
 
         self.image_label = QLabel()
@@ -23,33 +23,54 @@ class MainWindow(QMainWindow):
         self.last_point = QPoint()
         self.points = []
 
-        # self.init_ui()
+        self.init_ui()
 
-    # def init_ui(self):
-    #     # Create Menu Bar
-    #     menubar = self.menuBar()
-    #     file_menu = menubar.addMenu('File')
+    def init_ui(self):
+        # Create Menu Bar
+        menubar = self.menuBar()
+        file_menu = menubar.addMenu('File')
 
-    #     # Add Open Action
-    #     open_action = QAction('Open Image', self)
-    #     open_action.triggered.connect(self.open_image)
-    #     file_menu.addAction(open_action)
+        # Add Open Action
+        open_action = QAction('Open Image', self)
+        open_action.triggered.connect(self.open_image)
+        file_menu.addAction(open_action)
 
-    #     # Add Save Action
-    #     save_action = QAction('Save Mask', self)
-    #     save_action.triggered.connect(self.save_mask)
-    #     file_menu.addAction(save_action)
+        # Add Save Action
+        save_action = QAction('Save Mask', self)
+        save_action.triggered.connect(self.save_mask)
+        file_menu.addAction(save_action)
 
-    #     # Create Toolbar
-    #     toolbar = QToolBar()
-    #     self.addToolBar(toolbar)
+        # Create Toolbar
+        toolbar = QToolBar()
+        self.addToolBar(toolbar)
 
-    #     # Add Actions to Toolbar
-    #     toolbar.addAction(open_action)
-    #     toolbar.addAction(save_action)
+        # Add Actions to Toolbar
+        toolbar.addAction(open_action)
+        toolbar.addAction(save_action)
 
-    #     # Create Status Bar
-    #     self.statusBar = QStatusBar()
-    #     self.setStatusBar(self.statusBar)
+        # Create Status Bar
+        self.statusBar = QStatusBar()
+        self.setStatusBar(self.statusBar)
 
-    # ... (Additional methods will be added here)
+    def open_image(self):
+        options = QFileDialog.Options()
+        file_name, _ = QFileDialog.getOpenFileName(
+            self, "Open Image File", "", "Images (*.png *.jpg *.bmp *.tif)", options=options)
+        if file_name:
+            self.image = cv2.imread(file_name)
+            self.display_image()
+            self.points = []
+            self.mask = None
+            self.statusBar.showMessage(f"Opened image: {file_name}")
+
+    def save_mask(self):
+        if self.mask is None:
+            QMessageBox.warning(self, "Warning", "No mask to save.")
+            return
+        options = QFileDialog.Options()
+        file_name, _ = QFileDialog.getSaveFileName(
+            self, "Save Mask Image", "", "PNG Files (*.png)", options=options)
+        if file_name:
+            cv2.imwrite(file_name, self.mask)
+            self.statusBar.showMessage(f"Saved mask: {file_name}")
+
